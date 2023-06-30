778939e35a09f74835aba2ec0f5b71bec70b79ee
Maksim Balantsevich <M.Balantsevich@gmail.onmicrosoft.com>
2023-06-30 20:47:06 +0300

    emit events

--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
-var cors = require('cors')
+var cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 const posts = {};
 
@@ -13,7 +14,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -21,6 +22,14 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
