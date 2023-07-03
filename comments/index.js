e04ed9188c10780b73396db43f3363ac84d9dfc5
Maksim Balantsevich <M.Balantsevich@gmail.onmicrosoft.com>
2023-07-03 14:35:34 +0300

    add comment moderation

--- comments/index.js
+++ comments/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
-var cors = require('cors')
-const axios = require('axios');
+var cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -14,13 +14,13 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", async(req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -29,18 +29,41 @@ app.post("/posts/:id/comments", async(req, res) => {
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: "pending",
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Recived Event', req.body.type);
+app.post("/events", async (req, res) => {
+  console.log("Recived Event", req.body.type);
 
-  res.send({})
-})
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
+});
 
 app.listen(4001, () => {
   console.log("listinig port 4001");
